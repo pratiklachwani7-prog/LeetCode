b5d21be722b32c5d360ea62e91516872f6ca4108
pratiklachwani7-prog <pratiklachwani7@gmail.com>
2026-09-17 22:46:20 +0530

    Time: 44 ms (14.05%) | Memory: 25.3 MB (46.13%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxDistinct(string s) 
+    {
+        unordered_map<char,int> mp ;
+        for ( int i = 0 ; i < s.size() ; i++ ) mp[s[i]]++;
+        return mp.size() ;
+    }
+};
